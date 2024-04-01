dcc80aa5a8922db8f2d1052c0cd4e6d6a720469a
kcherechecha <kcherechecha@gmail.com>
2024-04-01 19:46:24 +0300

    added service interfaces

---
+++ FlowerShop.BLL/Interfaces/Services/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.BLL.Interfaces.Services
+{
+    public interface ICrud<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<Guid> AddAsync(T model); 
+        Task DeleteAsync(Guid id);
+        Task UpdateAsync(T model);
+    }
+}
