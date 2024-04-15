7fb321d216d0889d7c73e09aa0a43d424da4434f
kcherechecha <kcherechecha@gmail.com>
2024-04-15 21:53:24 +0300

    inject business services

--- FlowerShop.BLL/DependencyInjection.cs
+++ FlowerShop.BLL/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using FlowerShop.DAL.Data;
+﻿using FlowerShop.BLL.Interfaces.Services;
+using FlowerShop.BLL.Services;
+using FlowerShop.DAL.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +16,11 @@ namespace FlowerShop.BLL
         public static IServiceCollection AddBusinessServices(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(AutomapperProfile).Assembly);
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ICustomBouquetService, CustomBouquetService>();
+            services.AddScoped<IItemService, ItemService>();
+            services.AddScoped<IOrderService, OrderService>();
 
             return services;
         }
--- FlowerShop.WebAPI/Program.cs
+++ FlowerShop.WebAPI/Program.cs
@@ -1,12 +1,15 @@
+using FlowerShop.BLL;
 using FlowerShop.DAL.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDALServices(builder.Configuration);
+builder.Services.AddBusinessServices();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDALServices(builder.Configuration);
 
 var app = builder.Build();
 
