f7f24caed550c1b76d0affc69f6b3d70d70b88dc
kcherechecha <kcherechecha@gmail.com>
2024-04-15 20:10:14 +0300

    Input and View models

---
+++ FlowerShop.BLL/Models/InputModels/CustomBouquetInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.BLL.Models.InputModels
+{
+    public class CustomBouquetInputModel
+    {
+        public Guid Id { get; }
+        public byte[]? Photo { get; }
+        public Guid UserId { get; }
+        public string? UserDescription { get; }
+        public DateTime RequestTime { get; private set; } = DateTime.Now;
+    }
+}
