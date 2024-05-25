4ff102fc14144a172a2e7d6adec143084b2b0456
kcherechecha <kcherechecha@icloud.com>
2024-05-25 20:20:14 +0300

    validation fix

--- FlowerShop.UI/wwwroot/js/order/totalPrice.js
+++ FlowerShop.UI/wwwroot/js/order/totalPrice.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     updateTotalPrice();
 });
 
+function validateQuantity(input) {
+    const min = parseInt(input.min);
+    const max = parseInt(input.max);
+    const value = parseInt(input.value);
+
+    if (value < min) {
+        input.value = min;
+    }
+    if (value > max){
+        input.value = max
+    }
+}
+
 function calculateTotalPrice() {
     var total = 0;
     var items = document.getElementsByClassName('innerproductsection');
@@ -11,6 +24,7 @@ function calculateTotalPrice() {
         var price = parseFloat(priceElement.replace(' грн', ''));
 
         var quantityElement = items[i].getElementsByClassName('item-quantity')[0];
+        validateQuantity(quantityElement);  // Ensure quantity is at least 1
         var quantity = parseInt(quantityElement.value);
 
         total += price * quantity;
@@ -24,7 +38,6 @@ function updateTotalPrice() {
     var totalPriceElement = document.getElementById('totalPrice');
     totalPriceElement.textContent = 'Total Price: ' + totalPrice + ' uah';
 
-    var totalPrice = calculateTotalPrice();
     var totalPriceInput = document.getElementById('totalPriceInput');
     totalPriceInput.value = totalPrice;
 }
@@ -36,6 +49,7 @@ function orderItems() {
 
     for (var i = 0; i < items.length; i++) {
         var quantityElement = items[i].getElementsByClassName('item-quantity')[0];
+        validateQuantity(quantityElement);  // Ensure quantity is at least 1
         var quantity = parseInt(quantityElement.value);
         var itemOrderId = items[i].getAttribute('data-itemOrderId');
 
@@ -52,12 +66,13 @@ function orderItems() {
     $.ajax({
         type: 'PATCH',
         url: Url,
-        contentType: 'application/json', 
+        contentType: 'application/json',
         data: JSON.stringify(itemsCountUpdate),
         success: function (response) {
             console.log(`Added to order`);
         },
         error: function (xhr, status, error) {
+            console.error(`Error: ${error}`);
         }
     });
 }
